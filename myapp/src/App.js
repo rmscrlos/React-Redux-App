201077b81be19001e21111aa2600c2cf8e5da52a
Jean Carlos  Ramos <rmscrlos@gmail.com>
2021-01-05 23:02:16 -0500

    added form

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
+import AppInput from './components/AppForm';
 
 //components
 import AppTitle from './components/AppTitle';
 import InfoContainer from './components/InfoContainer';
+import AppForm from './components/AppForm';
 
 function App() {
 	return (
 		<div className="App">
 			<div className="container">
-				<div>
+				<div className="right__side">
 					<AppTitle />
 					<h6>Search up your favorite crypto.</h6>
+					<AppForm />
 				</div>
 				<InfoContainer />
 			</div>
---
+++ myapp/src/components/AppForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function AppForm(props) {
+	return (
+		<div className="form">
+			<form>
+				<input type="text" />
+				<button type="submit">search</button>
+			</form>
+		</div>
+	);
+}
+
+export default AppForm;
